1e7e8e21f56dd33af5ea8225de5ff020fa593f62
kcq888 <kcq@yahoo.com>
2019-11-13 10:23:20 -0500

    added sign out status

--- AttendantMain.py
+++ AttendantMain.py
@@ -9,79 +9,33 @@ from google.cloud import storage, exceptions
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import QDateTime, QTimeZone, Signal, Slot
 # Application Imports
-import Attendant
-
-class AttnSignal(QObject):
-    signal = Signal(str)
-
-class SignInOut(QObject):
-    Season = "S2019-2020"
-    SignIn = "SignIn"
-    SignOut = "SignOut"
-    def __init__(self):
-        self.cred = credentials.Certificate("team151attendant-firebase-adminsdk-6n2zi-b3551c705a.json")
-        firebase_admin.initialize_app(self.cred)
-        self.db = firestore.client()
-        self.reportstatus = AttnSignal()
-    
-    @Slot(str)
-    def process(self, rfid):
-        members = self.db.collection(u'members')
-        docref = members.document(rfid)
-        try:
-            doc = docref.get()
-            if (doc.exists):
-                name = doc.get(u'First') + " " + doc.get(u'Last')
-                self.reportstatus.signal.emit(name)
-                attnhistory = docref.collection(u'AttnHistory')
-                now = QDateTime().currentDateTimeUtc()
-                logdate = "Log" + "{}{}{}".format(now.date().month(),now.date().day(),now.date().year())
-                signdatetime = now.toPython()
-                if attnhistory is not None:
-                    signtype = self.SignIn
-                    doc = attnhistory.document(self.Season).get()
-                    if not doc.exists:
-                        attnhistory.document(self.Season).set({
-                            logdate : {
-                                signtype : signdatetime
-                            }
-                        })
-                    else:
-                        data = doc.get(logdate + "." + self.SignIn)
-                        prevsignin = datetime.datetime(data.year, data.month, data.day, data.hour, data.minute, data.second)
-                        delta = signdatetime - prevsignin
-                        if (delta.total_seconds() < 24*60*60):
-                            signtype = self.SignOut
-                        else:
-                            signtype = self.SignIn
-                        try:
-                            data = doc.get(logdate + "." + self.SignOut)
-                        except:
-                            docref = attnhistory.document(self.Season)
-                            docref.update({
-                                logdate + "." + signtype : signdatetime
-                            })
-            else:
-                self.reportstatus.signal.emit("RFID # unassigned")
-        except exceptions.NotFound:
-            print("No RFID document found!")
+from Attendant import Attendant
+from SignInOut import SignInOut
+from AttnSignal import AttnSignal
 
 class RfID(QtCore.QThread):
     def __init__(self):
-        self.signal = AttnSignal()
+        self.attnsignal = AttnSignal()
 
     def run(self):
-        with open('/dev/tty0', 'r') as tty:
-            while True:
-                RFID_input = tty.readline()
-                self.signal(RFID_input)
+        self.attnsignal.signal.emit("3699761165")
+        # with open('/dev/tty0', 'r') as tty:
+        #     while True:
+        #         RFID_input = tty.readline()
+        #         self.signal(RFID_input)
 
 if __name__ == "__main__":
     timesFont = QtGui.QFont("Times", 24, QtGui.QFont.Bold)
     app = QtWidgets.QApplication(sys.argv)
     app.setFont(timesFont)
     attendant = Attendant()
+    signinout = SignInOut()
+    signinout.reportname.signal.connect(attendant.updateName)
+    signinout.reportstatus.signal.connect(attendant.updateStatus)
     attendant.show()
+    rfid = RfID()
+    rfid.attnsignal.signal.connect(signinout.process)
+    rfid.run()
     app.exec_()
 
 
